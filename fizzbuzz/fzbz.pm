02e51b4bf54e78b6e267be79c8a39f9aaa1a3548
oat@abctech-thailand.com <theerapat@theerapat-PC.(none)>
2012-09-05 10:03:10 +0700

    Done code to match first test requirement.

--- fizzbuzz/fzbz.pm
+++ fizzbuzz/fzbz.pm
@@ -8,9 +8,23 @@ sub run_tests {
     is(fizzbuzz(1), 1, 'speak number by 1');
     is(fizzbuzz(2), 2, 'speak number by 2');
 
-    done_testing(8);
+    done_testing(2);
 
     exit;
 }
 
+sub main {
+	my $lim = int($ARGV[0] || 33);
+	for (1..$lim) {
+		print "$_: " . fizzbuzz($_) . "\n";
+	}
+}
+
+sub fizzbuzz {
+	my $num = $_[0];
+	return "$num";
+}
+
 run_tests if $ENV{HARNESS_ACTIVE};
+
+main;
