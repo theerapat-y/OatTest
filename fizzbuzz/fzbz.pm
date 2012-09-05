609cb96d93f10866f000d81965c0f5a94ec1392f
oat@abctech-thailand.com <theerapat@theerapat-PC.(none)>
2012-09-05 09:48:15 +0700

    Start write test.

--- fizzbuzz/fzbz.pm
+++ fizzbuzz/fzbz.pm
@@ -1,8 +1,16 @@
-#!usr/bin/perl
-
-package fizzbuzz::fb;
-
+#!/usr/bin/perl
 use strict;
 use warnings;
 
-1;
+use Test::More;
+
+sub run_tests {
+    is(fizzbuzz(1), 1, 'speak number by 1');
+    is(fizzbuzz(2), 2, 'speak number by 2');
+
+    done_testing(8);
+
+    exit;
+}
+
+run_tests if $ENV{HARNESS_ACTIVE};
