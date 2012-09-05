8be8914550db5005bcdb500e53663061e5f5587f
oat@abctech-thailand.com <theerapat@theerapat-PC.(none)>
2012-09-05 17:07:53 +0700

    Done coding to pass test4

--- fizzbuzz2/lib/fzbz.pm
+++ fizzbuzz2/lib/fzbz.pm
@@ -7,6 +7,8 @@ use warnings;
 sub fizzbuzz {
 	my $num = $_[0];
 	my $is_fizz = ($num % 3 == 0);
+	my $is_buzz = ($num % 5 == 0);
+	return 'buzz' if $is_buzz;
 	return 'fizz' if $is_fizz;
 	return shift;
 }
