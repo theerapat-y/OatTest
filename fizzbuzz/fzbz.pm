0ff946178d7d0d44079b4277cf11cfdabf9c5f26
oat@abctech-thailand.com <theerapat@theerapat-PC.(none)>
2012-09-05 10:16:42 +0700

    Done coding to match test4(check fizzbuzz) requirement.

--- fizzbuzz/fzbz.pm
+++ fizzbuzz/fzbz.pm
@@ -33,6 +33,7 @@ sub fizzbuzz {
 	my $num = $_[0];
 	my $is_fizz = ($num % 3 == 0);
 	my $is_buzz = ($num % 5 == 0);
+	return "fizzbuzz" if ($is_fizz && $is_buzz);
 	return "buzz" if $is_buzz;
 	return "fizz" if $is_fizz;
 	return "$num";
