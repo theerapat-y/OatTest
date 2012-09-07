be8ba842475e293c8d4d30ee5d6973165c26569c
oat@abctech-thailand.com <theerapat@theerapat-PC.(none)>
2012-09-07 10:22:08 +0700

    Done coding to pass test3

--- romannum/lib/romannum.pm
+++ romannum/lib/romannum.pm
@@ -5,6 +5,11 @@ sub romannumber {
 	my $num = shift;
 	return 1 if $num eq 'I';
 	return 5 if $num eq 'V';
+	return 10 if $num eq 'X';
+	return 50 if $num eq 'L';
+	return 100 if $num eq 'C';
+	return 500 if $num eq 'D';
+	return 1000 if $num eq 'M';
 }
 
 1;
