fa93951569ddbca88c48ce37835cc05432d65629
oat@abctech-thailand.com <theerapat@theerapat-PC.(none)>
2012-09-11 17:28:15 +0700

    Move algorithm in convert_decimal sub to newly created convert_single sub. Done convert_decimal to pass test3.

--- romannum2/lib/RomanNumeral.pm
+++ romannum2/lib/RomanNumeral.pm
@@ -9,13 +9,25 @@ has 'roman_number' => (is => 'rw', isa => 'Str');
 
 sub convert_decimal {
 	my $self = shift;
-	return 1 if $self->roman_number eq 'I';
-	return 5 if $self->roman_number eq 'V';
-	return 10 if $self->roman_number eq 'X';
-	return 50 if $self->roman_number eq 'L';
-	return 100 if $self->roman_number eq 'C';
-	return 500 if $self->roman_number eq 'D';
-	return 1000 if $self->roman_number eq 'M';
+	my @roman_char = split(//, $self->roman_number);
+	my $answer;
+
+	foreach my $char (@roman_char) {
+		$answer += convert_single($char);
+	}
+
+	return $answer;
+}
+
+sub convert_single {
+	my $input_number = shift;
+	return 1 if $input_number eq 'I';
+	return 5 if $input_number eq 'V';
+	return 10 if $input_number eq 'X';
+	return 50 if $input_number eq 'L';
+	return 100 if $input_number eq 'C';
+	return 500 if $input_number eq 'D';
+	return 1000 if $input_number eq 'M';
 }
 
 1;
