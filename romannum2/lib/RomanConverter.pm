dba702f0521b3cb5f9d35d9989215676ae5ee14d
oat@abctech-thailand.com <theerapat@theerapat-PC.(none)>
2012-09-11 15:58:28 +0700

    Call RomanNumeral class in RomanConverter. Test2 passed.

--- romannum2/lib/RomanConverter.pm
+++ romannum2/lib/RomanConverter.pm
@@ -2,11 +2,15 @@ package RomanConverter;
 
 use strict;
 use warnings;
+use RomanNumeral;
 
 use Sub::Exporter -setup => { exports => [ qw(convertnumber) ] };
 
 sub convertnumber {
-    return 1;
+	my $roman_num = shift;
+    my $numeral = RomanNumeral->new({ roman_number => $roman_num });
+
+    return $numeral->convert_decimal;
 }
 
 1;
