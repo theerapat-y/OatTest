89e5fc4c7702f33012043b3d9b2cde371700c544
oat@abctech-thailand.com <theerapat@theerapat-PC.(none)>
2012-09-14 13:53:06 +0700

    Added pod to RomanConverter module.

--- romannum2/lib/RomanConverter.pm
+++ romannum2/lib/RomanConverter.pm
@@ -6,11 +6,40 @@ use RomanNumeral;
 
 use Sub::Exporter -setup => { exports => [ qw(convertnumber) ] };
 
+=head1 NAME
+
+RomanConverter - module
+
+=head1 DESCRIPTION
+
+Module use as converter
+
+=head1 METHODS
+
+=cut
+
+=head2 convertnumber
+
+call RomanNumeral to convert input roman number to decimal.
+
+=cut
+
 sub convertnumber {
 	my $roman_num = shift;
     my $numeral = RomanNumeral->new();
 
     return $numeral->convert_decimal($roman_num);
 }
+
+=head1 AUTHOR
+
+Theerapat Yimsirikul,,,
+
+=head1 LICENSE
+
+This library is free software. You can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
 
 1;
