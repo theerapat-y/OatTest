22f06450e6865302780d8fd91cbaf92ab2caca9f
oat@abctech-thailand.com <theerapat@theerapat-PC.(none)>
2012-09-14 14:10:54 +0700

    Added pod to main.pl

--- romannum2/scripts/main.pl
+++ romannum2/scripts/main.pl
@@ -5,6 +5,16 @@ use warnings;
 
 use RomanConverter qw(convertnumber);
 
+=head1 NAME
+
+main
+
+=head1 DESCRIPTION
+
+Main script
+
+=cut
+
 open FILE, "scripts/sample01" or die $!;
 my @entry = <FILE>;
 close FILE or die $!;
